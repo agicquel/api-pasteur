11c326987c72c779242979febe5c5ed0950853bf
Antoine Gicquel <antoine.gicquel@mailoo.org>
2019-10-17 13:10:28 +0200

    Revert "rollback, I need the log file"
    This reverts commit 422607d7cd56d335df73f8ebe91f8c701bd6dab4.

--- app/routes/loraRoutes.js
+++ app/routes/loraRoutes.js
@@ -2,13 +2,12 @@ const router = require('express').Router();
 var mongoose = require('mongoose');
 var Display = mongoose.model('Display');
 const loraController = require('../controllers/loraController');
-const auth = require('../controllers/auth');
 const util = require('util')
 var log4js = require('log4js');
 var logger = log4js.getLogger('console');
 
-router.post('/', auth.validateUser, async (req, res) => {}
-/*
+router.post('/', loraController.loraValidate, async (req, res) => {
+
     try {
         if(typeof req.body.data.parsedData.esp_subscribed === 'undefined' || typeof req.body.data.parsedData.esp_not_sync === 'undefined') {
             logger.debug("Bad parameters");
@@ -62,8 +61,6 @@ router.post('/', auth.validateUser, async (req, res) => {}
         console.error(error);
         res.status(400).send("Error while processing");
     }
-})*/
-
-
+})
 
 module.exports = router
