98546c07db2e85643492cda9fc685cee7dce8718
Antoine Gicquel <antoine.gicquel@mailoo.org>
2019-10-31 18:36:23 +0100

    catch error processing

--- app/routes/loraRoutes.js
+++ app/routes/loraRoutes.js
@@ -73,7 +73,7 @@ router.post('/', loraController.loraValidate, async function (req, res) {
         res.end(JSON.stringify(responseStruct));
         res.end();
     } catch (error) {
-        console.error(error);
+        logger.debug("erreur processing : " + error);
         res.status(400).send("Error while processing");
     }
 });
